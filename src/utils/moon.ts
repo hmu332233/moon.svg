7c455dfd1c3c4679a95f5032ce17cc801e48b298
minung.han <hmu332233@gmail.com>
2022-03-10 01:36:00 +0900

    Add getMoonPhases function

---
+++ src/utils/moon.ts
@@ -0,0 +1,13 @@
+import {createTimeOfInterest} from 'astronomy-bundle/time';
+import {createMoon} from 'astronomy-bundle/moon';
+
+export const getMoonPhases = async () => {
+  const date = new Date();
+  const toi = createTimeOfInterest.fromDate(date);
+  const moon = createMoon(toi);
+  
+  const k = await moon.getIlluminatedFraction();
+  const isWaxing = await moon.isWaxing();
+
+  return { k, isWaxing };
+}
