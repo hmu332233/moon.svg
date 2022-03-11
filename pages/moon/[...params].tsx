e5a3ece7044ad14e01fb14f922ff4220c052f65f
minung.han <hmu332233@gmail.com>
2022-03-12 02:39:44 +0900

    meta 태그 이미지 주소 변경

--- pages/moon/[...params].tsx
+++ pages/moon/[...params].tsx
@@ -11,11 +11,11 @@ function Preview ({ params }: Props) {
   return (
     <Head>
       <title>Moon Phase ({date})</title>
-      <meta property="og:image" content={`/api/moon?date=${date}`} />
+      <meta property="og:image" content={`https://moon-phase.vercel.app/api/moon?date=${date}`} />
       <meta name="twitter:card" content="트위터 카드 타입(요약정보, 사진, 비디오)" /> 
       <meta name="twitter:title" content="콘텐츠 제목" /> 
       <meta name="twitter:description" content="웹페이지 설명" /> 
-      <meta name="twitter:image" content={`/api/moon?date=${date}`} /> 
+      <meta name="twitter:image" content={`https://moon-phase.vercel.app/api/moon?date=${date}`} /> 
     </Head>
   );
 }
