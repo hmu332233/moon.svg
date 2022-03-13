a69fd45f375f10108bd6866116867e5b1564532b
minung.han <hmu332233@gmail.com>
2022-03-14 02:40:38 +0900

    CodeBox 클릭시 내용 복사되도록 추가

--- components/CodeBox.tsx
+++ components/CodeBox.tsx
@@ -5,8 +5,15 @@ type Props = {
 };
 
 function CodeBox({ contents }: Props) {
+
+  const handleClick = () => {
+    navigator.clipboard.writeText(contents);
+    // TODO: 다른 UI로 대체
+    alert('Copied!')
+  };
+
   return (
-    <div className="mockup-code bg-base-300 text-base-content before:hidden my-4">
+    <div className="mockup-code bg-base-300 text-base-content cursor-pointer before:hidden my-4" onClick={handleClick}>
       <pre>
         <code>
           {contents}
