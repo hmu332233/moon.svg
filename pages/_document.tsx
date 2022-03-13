a0c23bf4e4ff53dd55dcce81ddf440740836bdb9
minung.han <hmu332233@gmail.com>
2022-03-14 02:44:13 +0900

    @next/next/next-script-for-ga lint 에러 나지않도록 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,8 @@ function Document() {
   return (
     <Html>
       <Head>
+        {/* TODO: Layout 쪽 로직으로 이동 */}
+        {/* eslint-disable-next-line @next/next/next-script-for-ga */}
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-LSQBD6ZK7N"
@@ -14,7 +16,6 @@ function Document() {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-            
               gtag('config', 'G-LSQBD6ZK7N');
             `,
           }}
