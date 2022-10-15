e4f5d42241de4bc08a139fc6bfec140a279053d6
Minung Han <hmu332233@gmail.com>
2022-10-16 02:38:28 +0900

    Add png api (#13)
    * resvg-js 추가
    
    * png api 추가
    
    * png api에서 api prefix 제거

---
+++ pages/api/moon.png/index.ts
@@ -0,0 +1,30 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import type { Theme } from 'server/theme/types';
+
+import createMoonFuncMap from 'server/theme';
+import { isTheme } from 'server/utils/type';
+import { renderPng } from 'server/utils/png';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { date, size = '100', theme = 'basic', rotate = '0' } = req.query;
+
+  if (!isTheme(theme)) {
+    return res.status(200).end('');
+  }
+
+  const createMoon = createMoonFuncMap[theme as Theme];
+  const moonSvg = await createMoon(
+    date as string,
+    size as string,
+    rotate as string,
+  );
+
+  res.setHeader('Content-Type', 'image/png');
+  res.setHeader('Cache-Control', 's-maxage=3600, max-age=3600');
+  res.status(200).end(renderPng(moonSvg));
+}
