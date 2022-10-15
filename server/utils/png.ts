1f80652a22c394bac1067f48f360caa6795a3fc0
Minung Han <hmu332233@gmail.com>
2022-10-16 00:17:49 +0900

    png api 추가

---
+++ server/utils/png.ts
@@ -0,0 +1,15 @@
+import { Resvg } from '@resvg/resvg-js';
+
+const opts = {
+  fitTo: {
+    mode: 'width',
+    value: 100,
+  },
+} as const;
+
+export const renderPng = (moonSvg: string) => {
+  const resvg = new Resvg(moonSvg, opts);
+  const pngData = resvg.render();
+  const pngBuffer = pngData.asPng();
+  return pngBuffer;
+};
