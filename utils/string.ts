88ff98da86137f5211977f97434a805b5295357d
Minung Han <hmu332233@gmail.com>
2022-12-04 17:55:28 +0900

    date가 없을때 query string에 undefined가 string으로 들어가던 버그 수정

--- utils/string.ts
+++ utils/string.ts
@@ -2,7 +2,7 @@ type QueryParameters = { [key: string]: any };
 export const objectToQueryString = (queryParameters: QueryParameters) => {
   return queryParameters
     ? Object.entries(queryParameters).reduce((queryString, [key, value]) => {
-        const encodedValue = encodeURIComponent(value);
+        const encodedValue = value && encodeURIComponent(value);
         const symbol = queryString.length === 0 ? '?' : '&';
         queryString += encodedValue ? `${symbol}${key}=${encodedValue}` : '';
         return queryString;
